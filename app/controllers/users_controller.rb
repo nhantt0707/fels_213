cff2d3b09f9926a3721621f6b6d20a48a5c45a3c
nhantt <nhant0707@gmail.com>
2016-11-25 15:50:48 +0700

    Resgister

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t "not_found"
+      redirect_to root_url
+    end
+  end
+
+  def index
+     @users = User.paginate page: params[:page]
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t("home_page.intro")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root  "static_pages#home"
   get "contact"  =>  "static_pages#contact"
+  get "signup"   =>  "users#new"
+  resources :users
 end
